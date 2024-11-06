2ad82bb804b103653725c9c9671c62ebf41a8218
Aidar Putilov <putiloff@gmail.com>
2024-11-07 02:54:27 +0500

    Release Generics

--- materials/urls.py
+++ materials/urls.py
@@ -1,6 +1,7 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonDestroyAPIView, LessonUpdateAPIView
 from materials.apps import MaterialsConfig
 
 
@@ -10,5 +11,9 @@ router = DefaultRouter()
 router.register(r"", CourseViewSet, basename="course")
 
 urlpatterns = [
-
+    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retrieve"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
+    path("lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"),
 ] + router.urls
