304848110c62cd43fd80197466f8294575ce754f
Aidar Putilov <putiloff@gmail.com>
2024-11-11 21:46:40 +0500

    Add django-filter

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,6 +4,11 @@ from materials.models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    # courses = serializers.SerializerMethodField()
+
+    # def get_courses(self, lesson):
+    #     return [course.name for course in Course.objects.filter(lesson=lesson)]
+
     class Meta:
         model = Lesson
         fields = "__all__"
